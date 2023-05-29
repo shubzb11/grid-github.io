f0f7e0c424df4d3ab30ae9c716c9d9f8e0a63888
shubzb11 <123492306+shubzb11@users.noreply.github.com>
2023-05-29 10:14:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import LatestPost from "./Components/LatestPost";
+function App() {
+  return (
+    <>
+      <Header />
+      <LatestPost />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
